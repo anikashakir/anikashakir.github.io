1c4d143a3d3d410d302783683b0963592f686da7
anikashakir <anikashakirs@yahoo.com>
2016-10-27 09:12:05 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php//Include the event calendar functions fileinclude_once('functions.php');?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>PHP Event Calendar by CodexWorld</title>
+    <!-- Include the stylesheet -->
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <!-- Include the jQuery library -->
+    <script src="jquery.min.js"></script>
+</head>
+<body>
+    <!-- Display event calendar -->
+    <div id="calendar_div">
+        <?php echo getCalender(); ?>
+    </div>
+</body>
+</html>
